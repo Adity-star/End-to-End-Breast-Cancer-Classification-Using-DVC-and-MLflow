1b034f2247aab27d0805d7d2799a708a8c3cc061
Aditya Akuskar <Adiiii@Aditya.Akuskar>
2025-05-05 01:07:40 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from BCClassifier import logger
+from BCClassifier import logger 
+from BCClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('Breast Cancer Classification Project')
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
